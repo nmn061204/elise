23626d995f825262b5928041e78eba8655d63c83
nmn061204 <22DH112366@st.huflit.edu.vn>
2023-10-23 23:27:56 +0700

    commit new 23/10 23h30

--- main.js
+++ main.js
@@ -11,30 +11,72 @@ window.addEventListener('scroll', () => {
 
 document.addEventListener("DOMContentLoaded", function () {
   // Lấy các phần tử HTML cần thiết
-  var modal = document.querySelector(".canvas-customer-login");
-  var modalMenu = document.querySelector(".header-canvas-menu");
-  var btn = document.querySelector(".header-account");
-  var overLay = document.querySelector(".header-overlay");
-  var btnClose = document.querySelector(".close");
-  var btnClose2 = document.querySelector(".header-overlay");
+// Lấy modal
+var modal = document.getElementsByClassName("canvas-customer-login")[0];
 
-  // Xử lý sự kiện khi người dùng nhấp vào nút mở cửa sổ đăng nhập
-  btn.addEventListener("click", function () {
-      modal.style.right = "0";
-      overLay.style.display = "block";
-  });
+var modalMenu = document.getElementsByClassName("header-canvas-menu")[0];
 
-  // Xử lý sự kiện khi người dùng nhấp vào nút đóng cửa sổ đăng nhập
-  btnClose.addEventListener("click", function () {
-      modal.style.right = "-500px";
-      overLay.style.display = "none";
-  });
+// Lấy button mở modal
+var btn = document.getElementsByClassName("header-account")[0];
 
-  // Xử lý sự kiện khi người dùng nhấp vào nút đóng cửa sổ đăng nhập hoặc nền đen mờ
-  btnClose2.addEventListener("click", function () {
-      modal.style.right = "-500px";
-      overLay.style.display = "none";
-  });
+var btnMenu = document.getElementsByClassName("action-menu-responsive")[0];
+
+
+var overLay = document.getElementsByClassName("header-overlay")[0];
+
+
+// Lay btn close modal
+
+var btnClose = document.getElementsByClassName("close")[0];
+var btnClose2 = document.getElementsByClassName("header-overlay")[0];
+
+var btnCloseMenu = document.getElementsByClassName("close-menu")[0];
+
+
+
+// Khi người dùng nhấp vào nút, mở modal
+btn.onclick = function () {
+  modal.style.right = "0";
+  overLay.style.display = "block";
+};
+
+btnMenu.onclick = function () {
+  modalMenu.style.left = "0";
+  overLay.style.display = "block";
+};
+
+
+// Khi người dùng nhấp vào nút, mở modal
+btnClose.onclick = function () {
+  modal.style.right = "-500px";
+  overLay.style.display = "none";
+
+};
+
+btnClose2.onclick = function () {
+  modal.style.right = "-500px";
+  overLay.style.display = "none";
+
+};
+
+btnCloseMenu.onclick = function () {
+  modalMenu.style.left = "-450px";
+  overLay.style.display = "none";
+
+};
+
+btnClose2.onclick = function () {
+  modal.style.right = "-500px";
+  modalMenu.style.left = "-450px";
+  overLay.style.display = "none";
+};
+
+
+
+
+
+
+
 
   // Xử lý slideshow
   let slideIndex = 0;
